c95fd990ca9b89fdc1055577eda54f13c084390a
MIIMOmama <tkokmsk@gmail.com>
2022-06-13 14:42:05 +0900

    ファーストビューswiper

--- _static/dist/js/index.js
+++ _static/dist/js/index.js
@@ -1,1 +1,14 @@
 AOS.init()
+
+let swipeOption = {
+    loop: true,
+    effect: 'fade',
+    autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+    },
+    speed: 2000,
+
+    }
+
+new Swiper('.swiper-container', swipeOption);
