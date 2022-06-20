c416e2fa1e19e0fe61b8d37203e08cf40198feec
MIIMOmama <tkokmsk@gmail.com>
2022-06-20 23:48:43 +0900

    スクロールjs追加

--- _static/dist/js/index.js
+++ _static/dist/js/index.js
@@ -11,3 +11,14 @@ let swipeOption = {
     }
 
 new Swiper('.swiper-container', swipeOption);
+
+
+
+window.addEventListener("scroll",function() {
+    const header = document.querySelector("header");
+    const h1 = document.querySelector("h1");
+    const navList = this.document.querySelector("#js-nav");
+    header.classList.toggle("header-scroll", window.scrollY>1);
+    h1.classList.toggle("headerLogo_text-scroll",window.scrollY>1);
+    navList.classList.toggle("globalNav_list-scroll",window.scrollY>1);
+});
